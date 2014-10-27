ffa61bdba69df0641cca3a445cb2a4e34d8773af
0u812 <0u812@github.com>
2014-10-27 11:02:43 -0700

    fix #8

--- src/model_getset.js
+++ src/model_getset.js
@@ -7,3 +7,23 @@ Object.defineProperty(Model.prototype, "species", {
     return result;
   }
 });
+
+Object.defineProperty(Model.prototype, "reactions", {
+  get: function reactions() {
+    var result = [];
+    for(var i=0; i<this.getNumReactions(); i++) {
+      result.push(this.getReaction(i));
+    }
+    return result;
+  }
+});
+
+Object.defineProperty(Model.prototype, "parameters", {
+  get: function parameters() {
+    var result = [];
+    for(var i=0; i<this.getNumParameters(); i++) {
+      result.push(this.getParameter(i));
+    }
+    return result;
+  }
+});
